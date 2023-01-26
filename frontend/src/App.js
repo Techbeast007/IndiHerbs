1c904316749f245a9cce18a85815ddaa9dd6a815
techbeast007 <navneetpandey34@gmail.com>
2023-01-26 17:23:13 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
-import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
@@ -62,7 +61,7 @@ const App = () => {
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
-      <Footer/>
+   
     </Router>
   )
 }
