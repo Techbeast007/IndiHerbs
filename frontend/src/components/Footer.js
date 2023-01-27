b3a585302df11c8f1cd0e391539ea51e11080c77
techbeast007 <navneetpandey34@gmail.com>
2023-01-27 09:31:33 +0530

    changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,41 +1,11 @@
+import React from 'react'
+import {  FiFacebook } from 'react-icons/fi';
 
-import { FaFacebookSquare} from 'react-icons/fa';
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FolderIcon from '@material-ui/icons/Folder';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
 import './Footer.css';
-
-const useStyles = makeStyles({
-  root: {
-    width: 500,
-  },
-});
-
-
 const Footer = () => {
-  
-  const classes = useStyles();
-  const [value, setValue] = React.useState('recents');
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
 
   return (
     <section className="footer">
-       <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
-      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-    </BottomNavigation>
-      
    
       <section className="footer-social-media">
         <a href="/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
@@ -63,7 +33,7 @@ const Footer = () => {
         </section>
         <section className="footer-info-right">
           <section className="footer-info__number">
-         <FaFacebookSquare/>
+         <FiFacebook/>
           </section>
           <section className="footer-info__contact">
             My Story
