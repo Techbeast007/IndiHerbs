71db7c1e3d68c06a0a89d337c921919b0995805c
techbeast007 <navneetpandey34@gmail.com>
2023-02-06 17:43:10 +0530

    changes

--- frontend/src/screens/singleBlog.js
+++ frontend/src/screens/singleBlog.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 import { useLocation } from 'react-router-dom'
+import Image from 'react-bootstrap/Image'
 
 function SingleBlog({id}){
     const [post,setPosts] = useState([])
@@ -18,10 +19,12 @@ function SingleBlog({id}){
     },[location.state.id])
     console.log(post)
     return (
-        <>
+        <><h1>{post.title}</h1>
+        <Image src={post.image} style={{width:"80vw",height:"400px"}}/>
+        <h6 style={{margin:"40px",display:"flex",justifyContent:"center"}}>{post.description}</h6>
        
        
-                <div>{post.title}</div>
+                
 
             
     
