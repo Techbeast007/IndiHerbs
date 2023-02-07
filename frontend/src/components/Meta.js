1ebcaf1f8a3f3828cc753e0cb9e6a42ba6965c56
techbeast007 <navneetpandey34@gmail.com>
2023-02-07 17:47:13 +0530

    changes

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Welcome To IndiHerbs',
+  description: 'We sell the best herbal supplements',
+  keywords: 'Indiherbs, Herbal Supplement, USA',
 }
 
 export default Meta
